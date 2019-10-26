da47cd2751c6ea85652c819bdc976ba236fbac72
AndreyShepkin <andreyshepkin47@gmail.com>
2019-10-26 17:50:11 +0300

    -

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
         # web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))
         start_webhook(
             dispatcher=dp,
-            webhook_path=WEBHOOK_URL,
+            webhook_path=WEBHOOK_PATH,
             on_startup=on_startup,
             on_shutdown=on_shutdown,
             skip_updates=True,
